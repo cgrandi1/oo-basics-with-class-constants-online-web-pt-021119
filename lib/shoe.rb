4fc28f2e8916938a4fb8fe2ed7a8c6c2848bd970
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-03-09 19:20:30 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,12 @@
+require 'pry'
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+
+  BRANDS = []
+  
   def initialize(brand)
     @brand = brand
   end
@@ -10,5 +15,10 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+  
+  def brand=(brand)
+    @brand = brand
+    BRANDS.push(brand) if brand.include?(brand)
+  end 
 
 end
